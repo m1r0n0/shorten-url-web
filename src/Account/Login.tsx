3b314dbb83230f2e5b90fed97e8973509e804dc9
m1r0n0 <Oleksandr.Myroniuk@udelphi.com>
2023-02-02 18:04:20 +0200

    brbrbrbrbrbr

--- src/Account/Login.tsx
+++ src/Account/Login.tsx
@@ -1,44 +1,61 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    rememberMe: false,
+  };
+
   render() {
     function handleSubmit(event: any) {
-      event.preventDefault();
-
       const data = new FormData(event.target);
 
       const value = Object.fromEntries(data.entries());
 
       console.log({ value });
     }
-
-    const form = document.querySelector("form");
-    // form.addEventListener("submit", handleSubmit);
     return (
       <div>
         <h2> Enter the app</h2>
-        <form>
-          <div asp-validation-summary="ModelOnly"></div>
-          <div>
-            <label htmlFor="email">Email</label> <br />
-            <input type="text" name="email" id="email" />
-          </div>
-          <div>
-            <label htmlFor="password"></label>Password <br />
-            <input type="password" name="password" id="password" />
-          </div>
-          <div>
-            <label htmlFor="rememberMe">Remember me?</label> <br />
-            <input type="checkbox" name="rememberMe" id="rememberMe" />
-          </div>
-          <div>
-            <input
-              type="submit"
-              value="Log in"
-              onClick={form!.handleSubmit(this.setState)}
-            />
-          </div>
-        </form>
+        <div asp-validation-summary="ModelOnly"></div>
+        <div>
+          <label htmlFor="email">Email</label> <br />
+          <input
+            value={this.state.email}
+            onChange={(event) =>
+              this.setState({ ...this.state, email: event.target.value })
+            }
+            type="text"
+            name="email"
+            id="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="password"></label>Password <br />
+          <input
+            onChange={(event) =>
+              this.setState({ ...this.state, password: event.target.value })
+            }
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+        <div>
+          <label htmlFor="rememberMe">Remember me?</label> <br />
+          <input
+            onChange={(event) =>
+              this.setState({ ...this.state, rememberMe: event.target.value })
+            }
+            type="checkbox"
+            name="rememberMe"
+            id="rememberMe"
+          />
+        </div>
+        <div>
+          <input type="button" value="Log in" onClick={handleSubmit} />
+        </div>
       </div>
     );
   }
