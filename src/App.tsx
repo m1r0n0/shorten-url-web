f281dcb291cad0624f86a40bb28634d3fdf8b118
m1r0n0 <Oleksandr.Myroniuk@udelphi.com>
2023-02-23 14:43:41 +0200

    context created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+import {
+  createContext,
+  useContext,
+  useState,
+  SetStateAction,
+  Dispatch,
+} from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {FMenu} from "./FMenu";
+import { FMenu } from "./FMenu";
+
+interface IUserIDContextType {
+  userID: undefined;
+  setUserID: Dispatch<SetStateAction<undefined>>;
+}
+
+const defaultUserIDContextValue = {
+  setUserID: () => undefined,
+  userID: undefined,
+};
+const UserIDContext = createContext<IUserIDContextType>(
+  defaultUserIDContextValue
+);
 
 function App() {
+  const [userID, setUserID] = useState();
   return (
     <div className="App">
       <header className="App-header">
+        {/* <UserIDContext.Provider
+          value={{
+            userID,
+            setUserID,
+          }}
+        >
+          
+        </UserIDContext.Provider> */}
         <FMenu />
       </header>
     </div>
